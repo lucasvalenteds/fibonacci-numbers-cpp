d2fe8fb57fd59b9f26afdefd263f5d00d2f52dd1
Lucas <lucasvalenteds@gmail.com>
2021-02-20 14:00:30 -0300

    Implementing CLI

--- Makefile
+++ Makefile
@@ -2,9 +2,12 @@ COMPILER := g++-10
 COMPILER_FLAGS := -g -Wall -O2
 LIBRARIES := -lgtest
 
+compile: main.cc
+	$(COMPILER) $(COMPILER_FLAGS) $^ -o $@
+
 test: main_test.cc
 	$(COMPILER) $(COMPILER_FLAGS) $^ -o $@ $(LIBRARIES)
 
 clean:
-	@rm -f test
+	@rm -f compile test
 
---
+++ main.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include "fib.cc"
+
+using namespace std;
+
+long interpret_user_input(int argc, char** argv) {
+	if (argc >= 2) {
+		long amount_of_numbers = atol(argv[1]);
+		cout << "Calculating " << amount_of_numbers << " fibonacci numbers" << endl;
+
+		return amount_of_numbers;
+	}
+
+	cout << "Missing amount of numbers to generate." << endl;
+	cout << "Usage: " << argv[0] << " <amount_of_numbers>" << endl;
+
+	return 0;
+}
+
+int main(int argc, char** argv) {
+	long amount_of_numbers = interpret_user_input(argc, argv);
+
+
+	for (auto &number : fibonacci_series(amount_of_numbers)) {
+		cout << number << endl;
+	}
+
+	return 0;
+}
+
